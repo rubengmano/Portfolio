3e4a2a04b4d970fe3013926b6b6016af8314d2dc
Rúben Mano <rumcmano@gmail.com>
2019-09-25 21:00:07 +0300

    Skills section code completed

--- js/script.js
+++ js/script.js
@@ -15,4 +15,48 @@ $(document).ready(function() {
         startDelay: 1000
     }); 
 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 4,
+        responsive:{
+            0:{
+                items:1
+            },
+            480:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            768:{
+                items:4 
+            }
+        }
+    });
+
+
+
+    // Take the position of the section to start the animation
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    //console.log(skillsTopOffset);
+
+    // Create a scroller event 
+    $(window).scroll(function() {
+        // it starts when it hits the window + 200 px
+        if(window.pageYOffset > skillsTopOffset - $(window).height() + 200){
+
+            $('.chart').easyPieChart({
+                easing:  'easeInOut',
+                barColor: '#fff',
+                trackColor: false,
+                scaleColor: false,
+                lineWidth: 4,
+                size: 152,
+                onStep: function(from, to, percent){
+                    $(this.el).find('.percent').text(Math.round(percent));
+                }
+            });
+        }
+    });
+
 });
